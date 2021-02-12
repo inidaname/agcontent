bae32550d6beeabfb8deeac2efadbf0f2797aef2
Hassan Sani <saniyhassan@gmail.com>
2021-02-12 12:16:14 +0100

    admin edited

--- src/app/home/branch/branch.component.ts
+++ src/app/home/branch/branch.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faBahai, faBuilding, faChartBar } from '@fortawesome/free-solid-svg-icons';
+import { faBahai, faBuilding, faChartBar, faMapMarker, faMapMarkerAlt, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   templateUrl: './branch.component.html',
@@ -11,9 +11,11 @@ export class BranchComponent implements OnInit {
   faLock = faBuilding;
   faBars = faChartBar;
   faBarSolid = faBahai;
+  faUser = faUser;
+  faBranch = faMapMarkerAlt;
 
   constructor() {
-    this.switchBranch = 'branchTable';
+    this.switchBranch = 'createBranch';
   }
 
   ngOnInit(): void {
