082fae242053b84980a330e6148b0fabde5222db
Hassan Sani <saniyhassan@gmail.com>
2021-02-23 12:41:42 +0100

    admin created

--- src/app/home/branch/branch.component.ts
+++ src/app/home/branch/branch.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { faBahai, faBuilding, faChartBar, faMapMarker, faMapMarkerAlt, faUser } from '@fortawesome/free-solid-svg-icons';
 import { Observable } from 'rxjs';
 import { ApiService } from 'src/app/shared/services/api.service';
@@ -17,15 +18,25 @@ export class BranchComponent implements OnInit {
   faUser = faUser;
   faBranch = faMapMarkerAlt;
   tableBranch: Observable<Food>;
+  listAdmins: Observable<Food>;
+  createBranchForm: FormGroup;
 
   constructor(
-    private api: ApiService
+    private api: ApiService,
+    private fb: FormBuilder
   ) {
     this.switchBranch = 'createBranch';
     this.tableBranch = this.api.getBranch();
+    this.listAdmins = this.api.getAllAdmins();
+    this.createBranchForm = this.fb.group({
+      admins: ['', Validators.required],
+      name: ['', Validators.required],
+      location: ['', Validators.required]
+    })
   }
 
   ngOnInit(): void {
+    this.tableBranch.subscribe(e => console.log(e));
   }
 
   setSwitch(switchState: string) {
